904e587000af953ee7bf39083fdd2a6d7f390ba5
ItzNxhin <nahinpenarandamejia@hotmail.com>
2023-11-11 13:17:33 -0500

    Calendario funcional

--- view/Reservas.java
+++ view/Reservas.java
@@ -3,14 +3,32 @@ package view;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import com.toedter.calendar.JDateChooser;
+import javax.swing.JButton;
+import javax.swing.SwingConstants;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class Reservas extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
+
+	public static void main(String[] args)  {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Reservas frame = new Reservas();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
 
 
 	public Reservas() {
@@ -25,6 +43,19 @@ public class Reservas extends JFrame {
 		JDateChooser dateChooser = new JDateChooser();
 		dateChooser.setBounds(93, 103, 160, 20);
 		contentPane.add(dateChooser);
+		
+		JButton btnPrueba = new JButton("Prueba");
+		btnPrueba.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				JOptionPane.showMessageDialog(null, dateChooser.getDate(), getTitle(), JOptionPane.INFORMATION_MESSAGE);
+			}
+		});
+		btnPrueba.setHorizontalAlignment(SwingConstants.RIGHT);
+		btnPrueba.setBounds(123, 148, 89, 23);
+		contentPane.add(btnPrueba);
+
+		
+	
 		
 		
 		
