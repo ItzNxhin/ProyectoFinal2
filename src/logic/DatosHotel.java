fa3747349e12a7b67f5eabac701cc702667539ce
ItzNxhin <nahinpenarandamejia@hotmail.com>
2023-11-11 19:56:45 -0500

    Datos

---
+++ src/logic/DatosHotel.java
@@ -0,0 +1,21 @@
+package logic;
+
+/**
+ * Esta clase maneja los siguientes datos del hotel
+ * Numero de habitaciones por cada tipo
+ * El precio de cada tipo de habitacion (Incluidos servicios)
+ * El precio de servicios adicionales
+ */
+public class DatosHotel {
+    public static int nPresidencial = 03;
+    public static int nPremium = 5;
+    public static int nVip = 10;
+    public static int nLite = 25;
+
+    public static double pPresidencial = 0;
+    public static double pPremium = 0;
+    public static double pVip = 0;
+    public static double pLite = 0;
+
+    
+}
