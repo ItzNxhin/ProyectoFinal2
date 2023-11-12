c51d3b83c937a34b2192f0b41bd06330e6da37e3
ItzNxhin <nahinpenarandamejia@hotmail.com>
2023-11-11 23:42:08 -0500

    Actualizacion HabVip

--- src/logic/HabVip.java
+++ src/logic/HabVip.java
@@ -3,6 +3,15 @@ package logic;
 public class HabVip extends HabAbstract{
     private String nombre = "Habitacion Lite";
     private int personas = 3;
+    private double precio = DatosHotel.pVip;
+
+    public double getPrecio() {
+        return this.precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
 
     public String getNombre(){
         return nombre;
