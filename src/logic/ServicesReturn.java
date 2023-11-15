bec418df423685c7668960c02588bbe75d718489
ItzNxhin <nahinpenarandamejia@hotmail.com>
2023-11-15 14:25:05 -0500

    Servicios dependiendo de la hab

---
+++ src/logic/ServicesReturn.java
@@ -0,0 +1,23 @@
+package logic;
+
+import java.util.ArrayList;
+
+public class ServicesReturn {
+    public ArrayList<Services> SerPorDefecto(HabAbstract hab){
+        ArrayList<Services> servicios = new ArrayList<>();
+        if(hab.getNombre().equals("Habitacion Presidencial")){
+
+        }
+        else if(hab.getNombre().equals("Habitacion Presidencial")){
+            
+        }
+        else if(hab.getNombre().equals("Habitacion Presidencial")){
+            
+        }
+        else {
+            
+        }
+        return servicios;
+    }
+    
+}
