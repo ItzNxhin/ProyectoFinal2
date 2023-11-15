095da79afd66b8b8a4518f7f3763090d7392a5f3
ItzNxhin <nahinpenarandamejia@hotmail.com>
2023-11-15 13:44:53 -0500

    Clases para servicios

---
+++ src/logic/SerJacuzzi.java
@@ -0,0 +1,17 @@
+package logic;
+
+public class SerJacuzzi implements Services{
+
+    @Override
+    public String getNombreSer() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getNombreSer'");
+    }
+
+    @Override
+    public int getPrecioSer() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getPrecioSer'");
+    }
+    
+}
