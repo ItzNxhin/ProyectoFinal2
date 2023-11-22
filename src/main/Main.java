a02e5cf2e9e9d3e5b565fb65051c0d7d610be0b9
ByJAXx <114113276+ByJAXx@users.noreply.github.com>
2023-11-22 18:57:48 -0500

    Inicia con la nueva interfaz

--- src/main/Main.java
+++ src/main/Main.java
@@ -11,18 +11,18 @@ import view.*;
 import java.awt.EventQueue;
 
 public class Main  {
-    public static void main(String[] args)  {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Principal frame = new Principal();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
+    EventQueue.invokeLater(new Runnable() {
+	public void run() {
+		try {
+			VentanaInicio frame = new VentanaInicio();
+			frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+			frame.setVisible(true);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
+	});
+}
 
      
 }
