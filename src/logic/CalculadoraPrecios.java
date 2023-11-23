0d4952ee788995fd9b9e40ca7c95084c4ab3a800
ByJAXx <114113276+ByJAXx@users.noreply.github.com>
2023-11-22 19:04:15 -0500

    Update CalculadoraPrecios.java

--- src/logic/CalculadoraPrecios.java
+++ src/logic/CalculadoraPrecios.java
@@ -2,7 +2,20 @@ package logic;
 
 public class CalculadoraPrecios {
 	//Este metodo ademas de el numero de dias debera recibir el precio por noche
-	public static double ValorReserva (long dias, double valorNoche) {
+	public static double ValorReserva (long dias, HabAbstract hab) {
+		double valorNoche = 0;
+		if(hab.getNombre().equals("Habitacion Presidencial")){
+            		valorNoche = DatosHotel.pPresidencial;
+	        }
+	        else if(hab.getNombre().equals("Habitacion Premium")){
+	        	valorNoche = DatosHotel.pPremium;
+	        }
+	        else if(hab.getNombre().equals("Habitacion Vip")){
+	        	valorNoche = DatosHotel.pVip;
+	        }
+	        else if(hab.getNombre().equals("Habitacion Lite")) {
+	        	valorNoche = DatosHotel.pLite;
+	        }
 		double precio = dias*valorNoche;
 		return precio;
 	}
@@ -19,7 +32,7 @@ public class CalculadoraPrecios {
 	public static double ValorServicio2 (boolean selected, long dias) {
 		double defaultPrice = 0;
 		if (selected) {
-			double valorServicio = 15;
+			double valorServicio = DatosHotel.sbuffet;
 			double precio = dias*valorServicio;
 			return precio;
 		}else {
@@ -29,7 +42,7 @@ public class CalculadoraPrecios {
 	public static double ValorServicio3 (boolean selected, long dias) {
 		double defaultPrice = 0;
 		if (selected) {
-			double valorServicio = 17;
+			double valorServicio = DatosHotel.sjacuzzi;
 			double precio = dias*valorServicio;
 			return precio;
 		}else {
@@ -40,7 +53,7 @@ public class CalculadoraPrecios {
 	public static double ValorServicio4 (boolean selected, long dias) {
 		double defaultPrice = 0;
 		if (selected) {
-			double valorServicio = 20;
+			double valorServicio = DatosHotel.slavanderia;
 			double precio = dias*valorServicio;
 			return precio;
 		}else {
@@ -51,7 +64,7 @@ public class CalculadoraPrecios {
 	public static double ValorServicio5 (boolean selected, long dias) {
 		double defaultPrice = 0;
 		if (selected) {
-			double valorServicio = 25;
+			double valorServicio = DatosHotel.stour;
 			double precio = dias*valorServicio;
 			return precio;
 		}else {
