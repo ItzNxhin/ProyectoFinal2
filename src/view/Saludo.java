0455a45de303c6baf8015bba728db2a892780713
ByJAXx <114113276+ByJAXx@users.noreply.github.com>
2023-11-26 12:40:36 -0500

    GUI Saludo.java

--- src/view/Saludo.java
+++ src/view/Saludo.java
@@ -24,21 +24,34 @@ public class Saludo extends JFrame {
 		
 		//Ventana
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 1366, 768);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
+		
+		// Cargar la imagen y configurar el fondo
+		ImageIcon imagenFondo = new ImageIcon("src/img/backgroundSaludo.png");
+		Image img = imagenFondo.getImage();
+		Image imgEscalada = img.getScaledInstance(getWidth(), getHeight(), Image.SCALE_SMOOTH);
+		ImageIcon imagenEscalada = new ImageIcon(imgEscalada);
+		JLabel etiquetaFondo = new JLabel(imagenEscalada);
+		etiquetaFondo.setBounds(0, 0, getWidth(), getHeight());
+		((JPanel) getContentPane()).setOpaque(false);
+		getLayeredPane().add(etiquetaFondo, new Integer(Integer.MIN_VALUE));
+		
 
 		//Saludo usuario
-		JLabel lblNewLabel = new JLabel("Hola usuario: " + current.getNombreUsuario());
-		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 15));
-		lblNewLabel.setBounds(106, 11, 238, 33);
+		JLabel lblNewLabel = new JLabel("Hola " + current.getNombreUsuario()+" ¿Listo para tener las vacaciones de tus sueños?");
+		lblNewLabel.setForeground(new Color(255, 255, 255));
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 30));
+		lblNewLabel.setBounds(222, 47, 1090, 46);
 		contentPane.add(lblNewLabel);
 		
 		//Boton reservar
 		JButton btnReservar = new JButton("Reservar");
+		btnReservar.setFont(new Font("Tahoma", Font.BOLD | Font.ITALIC, 30));
 		btnReservar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				Fechas fechas = new Fechas(current);
@@ -46,12 +59,17 @@ public class Saludo extends JFrame {
 				dispose();
 			}
 		});
-		btnReservar.setBounds(177, 190, 89, 23);
-		
-		
+		btnReservar.setBounds(683, 501, 218, 71);
+		// Establecer el fondo del botón como transparente
+		btnReservar.setOpaque(false);
+		// Hacer que el contenido del botón sea transparente
+		btnReservar.setContentAreaFilled(false);
+		btnReservar.setBorderPainted(false);
 		contentPane.add(btnReservar);
 		
-		JButton btnNewButton = new JButton("Ver reservas (Facturas)");
+		JButton btnNewButton = new JButton("Consultar reservas");
+		btnNewButton.setFont(new Font("Tahoma", Font.BOLD, 20));
+		btnNewButton.setBackground(new Color(208, 169, 51));
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				VerFacturas facturita = null;
@@ -65,23 +83,51 @@ public class Saludo extends JFrame {
 				dispose();
 			}
 		});
-		btnNewButton.setBounds(65, 190, 89, 23);
+		btnNewButton.setBounds(0, 231, 249, 80);
 		contentPane.add(btnNewButton);
-		/*
+		
 		JButton btnCambiarContrasea = new JButton("Cambiar contraseña");
+		btnCambiarContrasea.setFont(new Font("Tahoma", Font.BOLD, 20));
+		btnCambiarContrasea.setBackground(new Color(208, 169, 51));
 		btnCambiarContrasea.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				/*
 				String currentPassword = current.getContraseña();
 				CambiarContra cambiar = new CambiarContra(id, currentPassword);
 				cambiar.setVisible(true);
 				dispose();
+				*/
 			}
 		});
-		btnCambiarContrasea.setBounds(276, 190, 89, 23);
+		btnCambiarContrasea.setBounds(0, 310, 249, 80);
 		contentPane.add(btnCambiarContrasea);
-		 * 
-		 */
 		
+		JButton btnInfoHab = new JButton("Conoce las habitaciones");
+		btnInfoHab.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		btnInfoHab.setFont(new Font("Tahoma", Font.BOLD, 18));
+		btnInfoHab.setBackground(new Color(208, 169, 51));
+		btnInfoHab.setBounds(0, 153, 249, 80);
+		contentPane.add(btnInfoHab);
+		
+		JButton btnNewButton_1 = new JButton("Cerrar sesión");
+		btnNewButton_1.setBackground(new Color(208, 169, 51));
+		btnNewButton_1.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				int valor = JOptionPane.showConfirmDialog(null, "¿Estas seguro que quieres cerrar sesión?", "Atención", JOptionPane.YES_NO_OPTION);
+				if (valor == JOptionPane.YES_OPTION) {
+					VentanaInicio inicio = new VentanaInicio();
+					inicio.setVisible(true);
+					dispose();
+				}
+			}
+		});
+		btnNewButton_1.setFont(new Font("Tahoma", Font.BOLD, 18));
+		btnNewButton_1.setBounds(0, 389, 249, 80);
+		contentPane.add(btnNewButton_1);
 
 	}
 }
