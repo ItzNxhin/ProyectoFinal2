061eb582af626478f21ad1668e1def9a0256df42
ByJAXx <114113276+ByJAXx@users.noreply.github.com>
2023-11-26 17:37:57 -0500

    Nuevas ventanas de info

---
+++ src/view/InfoHab.java
@@ -0,0 +1,55 @@
+package view;
+
+import java.awt.EventQueue;
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import java.awt.Color;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class InfoHab extends JFrame {
+
+	private static final long serialVersionUID = 1L;
+	private JPanel contentPane;
+	/**
+	 * Create the frame.
+	 */
+	public InfoHab() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 1366, 768);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		
+		// Cargar la imagen y configurar el fondo
+		ImageIcon imagenFondo = new ImageIcon("src/img/habitaciones.png");
+		Image img = imagenFondo.getImage();
+		Image imgEscalada = img.getScaledInstance(getWidth(), getHeight(), Image.SCALE_SMOOTH);
+		ImageIcon imagenEscalada = new ImageIcon(imgEscalada);
+		JLabel etiquetaFondo = new JLabel(imagenEscalada);
+		etiquetaFondo.setBounds(0, 0, getWidth(), getHeight());
+		((JPanel) getContentPane()).setOpaque(false);
+		contentPane.setLayout(null);
+		
+		JButton btnNewButton = new JButton("Volver");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				dispose();
+			}
+		});
+		btnNewButton.setBackground(new Color(153, 102, 51));
+		btnNewButton.setBounds(1251, 11, 89, 23);
+		contentPane.add(btnNewButton);
+		getLayeredPane().add(etiquetaFondo, new Integer(Integer.MIN_VALUE));
+		
+		
+	}
+
+}
